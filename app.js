c502888c0248f2b9753a4c59b5630bacf0812fbb
violateer <1828257089@qq.com>
2020-12-11 17:19:57 +0800

    完善添加功能和路由参数

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const json = require('koa-json');
 const KoaRouter = require('koa-router');
 const path = require('path');
 const render = require('koa-ejs');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new KoaRouter();
 
 // json pretty
 app.use(json());
+
+app.use(bodyParser());
+
+// 给上下文ctx添加属性     即ctx.user
+app.context.user = 'violateer';
 
 // DB
 const things = [
@@ -27,12 +33,30 @@ render(app, {
 });
 
 // 路由跳转
-router.get('/', async ctx => {
+router.get('/', index);
+router.get('/add', showAdd);
+router.get('/test/:id', ctx => ctx.body = ctx.params.id);
+
+// 函数声明
+async function index (ctx) {
     await ctx.render('index', {
         title: 'Things i love...',
         things
     });
-});
+};
+
+async function showAdd (ctx) {
+    await ctx.render('add');
+};
+
+// 添加路由方法
+router.post('/add', add);
+
+async function add (ctx) {
+    const body = ctx.request.body;
+    things.push({ name: body.thing.toString() });
+    ctx.redirect(('/'));
+};
 
 // 配置路由模块
 app.use(router.routes()).use(router.allowedMethods());
