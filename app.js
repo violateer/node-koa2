e833eaa2fdc5449beb3c53490d8bc887b7c31a9c
violateer <1828257089@qq.com>
2020-12-11 15:34:59 +0800

    初始化路由和模板引擎

---
+++ app.js
@@ -0,0 +1,34 @@
+const Koa = require('koa');
+const json = require('koa-json');
+const KoaRouter = require('koa-router');
+const path = require('path');
+const render = require('koa-ejs');
+
+const app = new Koa();
+const router = new KoaRouter();
+
+// json pretty
+app.use(json());
+
+// 配置模板引擎
+render(app, {
+    root: path.join(__dirname, 'views'),
+    layout: 'layout',
+    viewExt: 'html',
+    cache: false,
+    debug: false
+});
+
+// 路由跳转
+router.get('/', async ctx => {
+    await ctx.render('index', {
+        title: 'Things i love...'
+    });
+});
+
+// 配置路由模块
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000, () => {
+    console.log('server start');
+});
